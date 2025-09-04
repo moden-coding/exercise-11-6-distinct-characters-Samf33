05fc840f4c662706b2a490f71468d6759c04d7a9
Samf33 <144719150+Samf33@users.noreply.github.com>
2025-09-04 11:25:06 -0400

    done

--- src/distinct_characters.py
+++ src/distinct_characters.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def distinct_characters(L):
-    pass
+    dic = {}
+    for i in L:
+        dic[i] = len(set(i))
+    return dic
 
 def main():
     print(distinct_characters(["check", "look", "try", "pop"]))
